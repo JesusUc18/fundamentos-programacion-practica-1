698cdf9d2a0e0375b655814fd7a9f5dad64cb9fe
JesusUc18 <jesus.uc@tecdesoftware.edu.mx>
2025-11-09 19:29:33 -0600

    Ejercicio #9 - Implementé un bucle for - Resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -141,7 +141,11 @@ function clasificarEdad(edad) {
  */
 function factorial(n) {
   // TODO: Implementa usando un bucle for
-  return 1;
+  let resultado = 1;
+  for (let i = 2; i <= n; i++) {
+    resultado *= i;
+  }
+  return resultado;
 }
 
 /**
